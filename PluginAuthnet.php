cb44e818b7c6fccea716f4b832252e76222ea5f8
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2014-06-03 16:30:49 -0500

    lang - replaced old comments marker /*T*/ with new one lang()

--- PluginAuthnet.php
+++ PluginAuthnet.php
@@ -15,85 +15,85 @@ class PluginAuthnet extends GatewayPlugin
               value       - default value
         */
         $variables = array (
-                   /*T*/"Plugin Name"/*/T*/ => array (
+                   lang("Plugin Name") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"How CE sees this plugin (not to be confused with the Signup Name)"/*/T*/,
-                                        "value"         =>/*T*/"Authorize.Net"/*/T*/
+                                        "description"   =>lang("How CE sees this plugin (not to be confused with the Signup Name)"),
+                                        "value"         =>lang("Authorize.Net")
                                        ),
-                   /*T*/"Authorize.Net API Login ID"/*/T*/ => array (
+                   lang("Authorize.Net API Login ID") => array (
                                         "type"          =>"text",
-                                        "description"   =>/*T*/"Please enter your Authorize.Net API Login ID Here."/*/T*/,
+                                        "description"   =>lang("Please enter your Authorize.Net API Login ID Here."),
                                         "value"         =>""
                                        ),
-                   /*T*/"Authorize.Net Transaction Key"/*/T*/ => array (
+                   lang("Authorize.Net Transaction Key") => array (
                                         "type"          =>"password",
-                                        "description"   =>/*T*/"Please enter your Authorize.Net Transaction Key Here."/*/T*/,
+                                        "description"   =>lang("Please enter your Authorize.Net Transaction Key Here."),
                                         "value"         =>""
                                        ),
-                   /*T*/"MD5 Hash Value"/*/T*/ => array (
+                   lang("MD5 Hash Value") => array (
                                         "type"          =>"password",
-                                        "description"   =>/*T*/"Used to verify valid transactions from Authorize.Net.<br>NOTE: This value has to match the value set in the Authorize.Net Merchant Interface - <i>Optional</i>"/*/T*/,
+                                        "description"   =>lang("Used to verify valid transactions from Authorize.Net.<br>NOTE: This value has to match the value set in the Authorize.Net Merchant Interface - <i>Optional</i>"),
                                         "value"         =>""
                                        ),
-                   /*T*/"Demo Mode"/*/T*/ => array (
+                   lang("Demo Mode") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES if you want to set this plugin in Demo mode for testing purposes."/*/T*/,
+                                        "description"   =>lang("Select YES if you want to set this plugin in Demo mode for testing purposes."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Accept CC Number"/*/T*/ => array (
+                   lang("Accept CC Number") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Selecting YES allows the entering of CC numbers when using this plugin type. No will prevent entering of cc information"/*/T*/,
+                                        "description"   =>lang("Selecting YES allows the entering of CC numbers when using this plugin type. No will prevent entering of cc information"),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Visa"/*/T*/ => array (
+                   lang("Visa") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow Visa card acceptance with this plugin.  No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow Visa card acceptance with this plugin.  No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"MasterCard"/*/T*/ => array (
+                   lang("MasterCard") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow MasterCard acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow MasterCard acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"AmericanExpress"/*/T*/ => array (
+                   lang("AmericanExpress") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow American Express card acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow American Express card acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Discover"/*/T*/ => array (
+                   lang("Discover") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow Discover card acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow Discover card acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Invoice After Signup"/*/T*/ => array (
+                   lang("Invoice After Signup") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES if you want an invoice sent to the customer after signup is complete."/*/T*/,
+                                        "description"   =>lang("Select YES if you want an invoice sent to the customer after signup is complete."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Signup Name"/*/T*/ => array (
+                   lang("Signup Name") => array (
                                         "type"          =>"text",
-                                        "description"   =>/*T*/"Select the name to display in the signup process for this payment type. Example: eCheck or Credit Card."/*/T*/,
+                                        "description"   =>lang("Select the name to display in the signup process for this payment type. Example: eCheck or Credit Card."),
                                         "value"         =>"Credit Card"
                                        ),
-                   /*T*/"Dummy Plugin"/*/T*/ => array (
+                   lang("Dummy Plugin") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"1 = Only used to specify a billing type for a customer. 0 = full fledged plugin requiring complete functions"/*/T*/,
+                                        "description"   =>lang("1 = Only used to specify a billing type for a customer. 0 = full fledged plugin requiring complete functions"),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Auto Payment"/*/T*/ => array (
+                   lang("Auto Payment") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"No description"/*/T*/,
+                                        "description"   =>lang("No description"),
                                         "value"         =>"1"
 
                                        ),
-                   /*T*/"30 Day Billing"/*/T*/ => array (
+                   lang("30 Day Billing") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Select YES if you want ClientExec to treat monthly billing by 30 day intervals.  If you select NO then the same day will be used to determine intervals."/*/T*/,
+                                        "description"   =>lang("Select YES if you want ClientExec to treat monthly billing by 30 day intervals.  If you select NO then the same day will be used to determine intervals."),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Check CVV2"/*/T*/ => array (
+                   lang("Check CVV2") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Select YES if you want to accept CVV2 for this plugin.  Please set your Authorize.net account to reject transactions with CVV2 mismatches."/*/T*/,
+                                        "description"   =>lang("Select YES if you want to accept CVV2 for this plugin.  Please set your Authorize.net account to reject transactions with CVV2 mismatches."),
                                         "value"         =>"1"
                                        )
 
